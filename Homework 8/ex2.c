95028a3884c01c89db869f67f9f788ee04fd9637
Robertron90 <35875418+Robertron90@users.noreply.github.com>
2018-10-11 15:44:30 +0000

    Add files via upload
    Homework of week08

---
+++ Homework 8/ex2.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+#include <time.h>
+
+
+
+int main(){
+
+  
+	for(int i=0; i < 10; i++){
+    
+		void* byte1 = malloc(sizeof(char)<<20);
+    
+		byte1 = memset(byte1, 0, sizeof(char)<<20);
+    
+		sleep(1);
+  
+	}
+
+
+
+}
